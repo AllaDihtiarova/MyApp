47e3cfc55bb6ba5394e7ab624a5d5dab560b5ead
AllaDihtiarova <AllaDihtiarova@gmail.com>
2022-10-21 08:21:31 +0300

    First commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ export const Counter = () => {
   
   // const [counter, setCounter] = useState(JSON.parse(window.localStorage.getItem("counter")) ?? 0);
   const [counter, setCounter] = useState(0)
-  const [disabled, setDisabled] = useState("Disabled")
+  const [disabled, setDisabled] = useState(true)
   
   const handleCounterIncrement = () => {
     setCounter(state => state + 1);
@@ -20,19 +20,19 @@ export const Counter = () => {
   }
 
   const handleDisabled = () => {
-    setDisabled("Enabled")
+    setDisabled(!disabled) 
   }
  
   return (
     <div style={{border: "2px solid black", width: "304px"}}>
     <div className={styles.container}>
       <button type="button" onClick={handleReset} className={styles.button} style={{ backgroundColor: "red" }}>Reset</button>
-      <button type="button" onClick={handleDisabled} className={styles.button} style={{ backgroundColor: "blue" }}>{ disabled}</button>
+      <button type="button" onClick={handleDisabled} className={styles.button} style={{ backgroundColor: "blue" }}>{ disabled ? "Disabled" : "Enabled"}</button>
     </div >
     <div className={styles.container}>
-      <button type="button" onClick={handleCounterDecrement} className={styles.counter} style={{ backgroundColor: "yellow", color: "white" }}>-</button>
-      <div className={styles.counter} style={{backgroundColor: (counter<0) ? "red" : "green"}}>{ counter }</div>
-      <button type="button" onClick={handleCounterIncrement} className={styles.counter} style={{ backgroundColor: "green", color: "white" }}>+</button>
+      <button type="button" onClick={disabled ? handleCounterDecrement : console.log("нажмите кнопку Enable")} className={disabled ? styles.counterDecrement : styles.toggle} >-</button>
+      <div className={styles.counter} style={{color: (counter<0) ? "red" : "green"}}>{ counter }</div>
+      <button type="button" onClick={disabled ? handleCounterIncrement : console.log("нажмите кнопку Enable")} className={disabled ? styles.counterIncrement : styles.toggle}>+</button>
       </div >
     </div>  
   );
